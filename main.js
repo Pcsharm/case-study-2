478bff094eee0d1c9309ee598bd80873f850a55c
prem <78126894+Pcsharm@users.noreply.github.com>
2021-08-23 18:39:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,23 +13,26 @@ camera = document.getElementById("camera");
 
 Webcam.attach('#camera');
 
-function take_snapshot(){
+function take_snapshot()
+{
 Webcam.snap(function(data_uri){
-    document.getElementById("result").innerHTML = 'img id= "captured_image"  src ="'+data_uri+'"/>';
+    document.getElementById("result").innerHTML = '<img id= "captured_image"  src ="'+data_uri+'"/>';
   
 
 });
 }
 console.log('ml5 version:',ml5.version);
-classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+classifier = ml5.imageClassifier('MobileNet', modelLoaded);
 
-function modelLoaded(){
+function modelLoaded()
+{
     console.log('Model Loaded!');
 
 }
-function check(){
+function check()
+{
     img = document.getElementById('captured_image');
-    classifier.classify('img, gotResult');
+    classifier.classify(img, gotResult);
 }
 function gotResult(error, results){
     if (error){
